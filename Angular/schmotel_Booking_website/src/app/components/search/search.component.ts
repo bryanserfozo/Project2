ede38be6b526a8cac7e81600184d0cae3f8fa63d
HelloNikoG <nikog.nyc@gmail.com>
2022-03-01 03:14:17 -0800

    added search component inputs

--- Angular/schmotel_Booking_website/src/app/components/search/search.component.ts
+++ Angular/schmotel_Booking_website/src/app/components/search/search.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { Search } from 'src/app/Interfaces/search';
 import { SearchresultService } from 'src/app/services/searchresult.service';
@@ -6,116 +7,47 @@ import { Destination } from 'src/app/Interfaces/destination';
 
 @Component({
   selector: 'app-search',
-  template: `
-    <section class="search-section">
-      <div class="form-wrapper">
-        <!-- location input section -->
-        <form class="hotel-search-form">
-          <div class="row">
-            <div class="four columns">
-              <label for="search-city-input"> Where to?</label>
-              <input
-                
-                type="text"
-                placeholder="e.g. North Korea"
-                id="search-city-input"
-                class="u-full-width search-city-input"
-              />
-            </div>
-
-            <!-- departing/arrival date calendar input section-->
-            <div class="five columns">
-              <div class="six columns">
-                <label for="check-in date">check-in</label>
-                <input
-                  type="date"
-                  id="check-in-date"
-                  class="u-full-width check-in-date"
-                />
-              </div>
-              <div class="six columns">
-                <label for="num-passengers">check-out</label>
-                <input
-                  type="date"
-                  id="check-out-date"
-                  class="u-full-width check-out-date"
-                />
-              </div>
-            </div>
-            <!--  -->
-            <!-- number of guests input section -->
-            <div class="three columns u-full-width guest-qty-input">
-              <div class="one column left-button">
-                <i class="fa-solid fa-angle-left"></i>
-              </div>
-              <div class="three columns">
-                <label for="num-guests">
-                  <i class="fa-solid fa-user-group"></i>
-                  <input type="number" id="num-guests" class="num-guests" />
-                </label>
-              </div>
-              <div class="one column right-button">
-                <i class="fa-solid fa-angle-right"></i>
-              </div>
-            </div>
-          </div>
-        </form>
-        <div class="search-btn-div">
-          <button
-            
-            class="button-primary search-btn"
-            id="seatch-btn"
-            type="submit"
-          >
-            search
-          </button>
-        </div>
-      </div>
-    </section>
-  `,
+  templateUrl: './search.component.html',
   styleUrls: ['./search.component.css'],
 })
-
-
-
 export class SearchComponent implements OnInit {
-  // constructor(private router: Router) {}
+  constructor(private http: HttpClient) {}
 
+  destination = '';
+  checkInDate = '';
+  checkOutDate = '';
+  numGuests = 2;
 
+  //submit the form and get values
+  onSubmit(data: any) {
+    console.log(data);
+    this.http.post('', data);
+  }
+  // plus/minus number of guests
+  public setNumGuest(type: string): void {
+    console.log('clicked ');
+    type === 'plus' ? this.numGuests++ : this.numGuests--;
+  }
 
   lstdestinations: Destination[] = [];
 
+  // constructor(private searchresultService: SearchresultService) {}
 
-  constructor(private searchresultService: SearchresultService) {
-
-  }
-
-    // searchreaultData = null;
-
+  // searchreaultData = null;
 
   ngOnInit(): void {
-  //   this.searchresultService.getsearchresult().subscribe((data)=>{
-
-  //     console.log(data);
-  //     this.searchreaultData = data;
-  //   });
+    //   this.searchresultService.getsearchresult().subscribe((data)=>{
+    //     console.log(data);
+    //     this.searchreaultData = data;
+    //   });
   }
 
   public callApi() {
-      console.log("the api has been called");
-      this.searchresultService.callApi().subscribe((data)=>{
-       console.log(data);
-        });
+    console.log('the api has been called');
+    // this.searchresultService.callApi().subscribe((data)=>{
+    //  console.log(data);
+    //   });
   }
 
- // searchFilter = false;
-
-
-  }
-
-  
-
- 
-  
-
-  
+  // searchFilter = false;
+}
--- Angular/schmotel_Booking_website/src/app/services/searchresult.service.ts
+++ Angular/schmotel_Booking_website/src/app/services/searchresult.service.ts
@@ -1,46 +1,37 @@
 import { Injectable } from '@angular/core';
-import {Observable} from "rxjs";
-import { HttpClient, HttpHeaders, HttpParams } from "@angular/common/http";
+import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Search } from '../Interfaces/search';
 import { catchError, Subject, throwError } from 'rxjs';
 
-
-
-
-
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class SearchresultService {
+  constructor(private http: HttpClient) {}
 
-  constructor(private httpclient: HttpClient) { }
+  //   getsearchresult(): Observable<any> {
+  //        console.log("request is send");
+  //          return this.httpclient.get("https://hotels4.p.rapidapi.com/properties/list");
+  //   }
 
+  // callApi(): Observable<any> {
+  //   const headers = new HttpHeaders()
+  //     .set('x-rapidapi-host', 'hotels4.p.rapidapi.com')
+  //     .set(
+  //       'x-rapidapi-key',
+  //       '1102d71a47msh51a4afddc13a729p1720b3jsn54bd62d2efb3'
+  //     );
 
-//   getsearchresult(): Observable<any> {
-//        console.log("request is send");
-//          return this.httpclient.get("https://hotels4.p.rapidapi.com/properties/list");
-//   }
+  //   const params = new HttpParams()
+  //     .set('query', 'new york')
+  //     .set('locale', 'en_US')
+  //     .set('currency', 'USD');
 
-
- callApi(): Observable<any> {
-
-  const headers = new HttpHeaders()
-  .set('x-rapidapi-host', 'hotels4.p.rapidapi.com')
-  .set('x-rapidapi-key', '1102d71a47msh51a4afddc13a729p1720b3jsn54bd62d2efb3');
-
-  
-
-  const params = new HttpParams()
-    .set('query', 'new york')
-    .set('locale', 'en_US')
-    .set('currency', 'USD');
-
-
-
-   console.log("request is send");
-   return this.httpclient.get("https://hotels4.p.rapidapi.com/locations/v2/search", {headers :headers, params: params});
-                    
-   }
-
+  //   console.log('request is send');
+  //   return this.httpclient.get(
+  //     'https://hotels4.p.rapidapi.com/locations/v2/search',
+  //     { headers: headers, params: params }
+  //   );
+  // }
 }
-
