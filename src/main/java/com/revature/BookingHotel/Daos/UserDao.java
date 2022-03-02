8300adf348123046af66a3df5e4b008ec94400d0
Mimi-AB <seunabimbola98@gmail.com>
2022-03-02 14:49:33 -0500

    User Dao

--- src/main/java/com/revature/BookingHotel/Daos/UserDao.java
+++ src/main/java/com/revature/BookingHotel/Daos/UserDao.java
@@ -1,4 +1,12 @@
 package com.revature.BookingHotel.Daos;
 
-public interface UserDao {
+import com.revature.BookingHotel.Models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserDao extends JpaRepository<User, Integer> {
+
+    User getUserByUsername(String email);
+
 }
