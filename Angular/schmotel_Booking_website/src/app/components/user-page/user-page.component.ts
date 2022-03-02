32117282c9653f757d903afcf12fe73cfcfe138a
Mimi-AB <seunabimbola98@gmail.com>
2022-03-02 16:31:21 -0500

    user stuff

--- Angular/schmotel_Booking_website/src/app/components/user-page/user-page.component.ts
+++ Angular/schmotel_Booking_website/src/app/components/user-page/user-page.component.ts
@@ -3,6 +3,7 @@ import { Observable, ObservableInputTuple } from 'rxjs';
 import { IUser } from 'src/app/Interfaces/IUser';
 import { Router } from '@angular/router';
 import { DataService } from 'src/app/services/data.service';
+import { UserServiceService } from 'src/app/services/user-service.service';
 
 @Component({
   selector: 'app-user-page',
@@ -13,9 +14,11 @@ export class UserPageComponent implements OnInit {
 
   users:Observable<IUser[]> = new Observable<IUser[]>()
 
-  hide:boolean = true;
+  hide:boolean = false;
+  hideform:boolean = false;
 
   user: IUser = {
+    id: 0,
     firstName : "",
     lastName : "",
     username : "",
@@ -25,11 +28,49 @@ export class UserPageComponent implements OnInit {
   }
 
 
+  updateFirstName: string = "";
+  updateLastName: string = "";
+  updateUsername: string = "";
+  updateEmail: string = "";
+  updatePassword: string = "";
+  updatePhoneNumber: string = "";
+
+
+
   showHidePaycardForm(): void{
     this.hide=!this.hide;
   }
 
-  constructor(private dataService:DataService) { }
+  show(): void{
+    this.hideform=!this.hideform;
+  }
+
+  updateUser(): void{
+    if(this.updateFirstName !== ""){
+      this.user.firstName = this.updateFirstName;
+    }
+    if(this.updateLastName !== ""){
+      this.user.lastName = this.updateLastName;
+    }
+    if(this.updateUsername !== ""){
+      this.user.username = this.updateUsername;
+    }
+    if(this.updateEmail !== ""){
+      this.user.email = this.updateEmail;
+    }
+    if(this.updatePassword !== ""){
+      this.user.password = this.updatePassword;
+    }
+    if(this.updatePhoneNumber !== ""){
+      this.user.phoneNumber = this.updatePhoneNumber;
+    }
+    this.userService.updateUser(this.user);
+    this.dataService.changeUser(this.user);
+    alert("updated");
+    this.router.navigate(['user']);
+  }
+
+  constructor(private dataService:DataService, private userService:UserServiceService, private router:Router) { }
 
   ngOnInit(): void {
     this.dataService.currentUser.subscribe(user => this.user = user)
