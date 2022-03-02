ec9689aa1448a0457464f179b8e7f7814059b664
bryanserfozo <bryan.serfozo@revature.net>
2022-03-02 13:54:46 -0500

    updating nav bar for login

--- Angular/schmotel_Booking_website/src/app/components/nav/nav.component.ts
+++ Angular/schmotel_Booking_website/src/app/components/nav/nav.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { IUser } from 'src/app/Interfaces/IUser';
+import { DataService } from 'src/app/services/data.service';
 
 
 @Component({
@@ -8,15 +10,34 @@ import { Router } from '@angular/router';
   styleUrls: ['./nav.component.css']
 })
 export class NavComponent implements OnInit {
+
+  signedIn: boolean = false;
+
+  navigateHome():void{
+    this.router.navigate(['home']);
+  }
+
+  
   
   navigateLogin():void{
+      this.dataService.changeUser({
+        id: 0,
+      email: '',
+      username: '',
+      firstName: '',
+      lastName: '',
+      password: '',
+      phoneNumber: '1234567891',
+      })
+      this.router.navigate(['login']);
+    }
+  constructor(private router:Router, private dataService:DataService){}
 
-    this.router.navigate(['login']);
-  }
-  constructor(private router:Router){}
 
-
-  ngOnInit(): void {
+  async ngOnInit(): Promise<void> {
+    console.log("Initialized")
+    this.dataService.currentSignedIn.subscribe(value=>this.signedIn = value)
+    
   }
 
 }
