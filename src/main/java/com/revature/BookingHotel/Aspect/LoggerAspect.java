ebc803dcb68c43cb0bd712d0bdb97fb2430abf6b
Warren <h.warren8888@gmail.com>
2022-03-04 11:28:12 -0500

    Logging Completed

---
+++ src/main/java/com/revature/BookingHotel/Aspect/LoggerAspect.java
@@ -0,0 +1,38 @@
+package com.revature.BookingHotel.Aspect;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class LoggerAspect {
+
+    public LoggerAspect() {
+    }
+
+    private Logger log = LoggerFactory.getLogger(LoggerAspect.class);
+
+    @Pointcut(value="execution(* com.revature.BookingHotel.*.*.*(..))")
+    public void myPointcut() {
+    }
+
+    @Around("myPointcut()")
+    public Object aroundLog(ProceedingJoinPoint pjp) throws Throwable {
+        ObjectMapper mapper = new ObjectMapper();
+        String methodName = pjp.getSignature().getName();
+        String className = pjp.getTarget().getClass().toString();
+        Object[] array = pjp.getArgs();
+        log.info("Method Invoked: [" + className + "]: " + methodName + "()" + " | Arguments: " + mapper.writeValueAsString(array));
+        Object object = pjp.proceed();
+        log.info("[" + className + "]: " + methodName + "()" + " | Response: " + mapper.writeValueAsString(object));
+        return object;
+    }
+}
